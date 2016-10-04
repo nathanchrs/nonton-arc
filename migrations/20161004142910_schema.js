d494ab0b475ac8a52f3abe261ca4b733e5edbbeb
Jonathan Christopher <jonathan.christopher@outlook.com>
2016-10-04 14:57:54 +0700

    Added DB schema migration

---
+++ migrations/20161004142910_schema.js
@@ -0,0 +1,30 @@
+
+exports.up = function (knex, Promise) {
+  return Promise.all([
+
+    knex.schema.createTable('series', function (table) {
+      table.increments('id').primary();
+      table.string('title');
+      table.integer('year');
+      table.string('genre');
+      table.string('synopsis');
+    }),
+
+    knex.schema.createTable('episodes', function (table) {
+      table.increments('id').primary();
+      table.integer('order');
+      table.string('title');
+      table.dateTime('releaseDate');
+      table.string('description');
+      table.string('path');
+    })
+
+  ]);
+};
+
+exports.down = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('series'),
+    knex.schema.dropTable('episodes')
+  ]);
+};
